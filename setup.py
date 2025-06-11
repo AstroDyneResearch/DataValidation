5d27d78e03340f76cb512ac657077512eb66ff3d
extra-medium-beats <dan.sabbatical@gmail.com>
2025-06-11 09:49:55 -0500

    Setup.py
    -- added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='DataValidation',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='AstroDyne Research',
+    author_email='',
+    description=''
+)
